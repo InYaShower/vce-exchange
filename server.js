f8c63a246a534bdfeeaca6b9c707919650a5e46c
Sathur <SUG0002@hcsc.vic.edu.au>
2019-08-15 20:17:54 +1000

    Site templated

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
-
-app.get('/', function(req, res) {
-    res.send('Hello world')
+app.use(express.static('static'))
+app.get('/', function (req, res) {
+    res.sendFile("static/home.html", { root: __dirname })
 })
-
 
 app.listen(80)
